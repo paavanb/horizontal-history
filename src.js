598f57a2ef27ed434f2a04d2b6fa443c383adf94
Paavan Bhavsar <pbhavsar@kyruus.com>
2016-07-06 20:17:42 -0400

    Migrated to d3 v4, added axis and margins

--- src.js
+++ src.js
@@ -1,35 +1,55 @@
 function type(d) {
-  d.birth = +d.birth; // coerce to number
-  d.death = +d.death;
+  d.birth = new Date(+d.birth, 0, 1)
+  d.death = new Date(+d.death, 0, 1)
   return d;
 }
 
+var margin = {
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 50
+}
 var width = 960, height = 500;
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], 0.1);
-var y = d3.scale.linear()
-    .range([height, 0]);
 
 var chart = d3.select('.chart')
-    .attr('width', width)
-    .attr('height', height);
+    .attr('width', width + margin.top + margin.bottom)
+    .attr('height', height + margin.left + margin.right)
+  .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.tsv("data.tsv", type, function(error, data) {
-    x.domain(d3.map(data, function(d) { return d.name; }));
+    var earliest_date = new Date(d3.min(data, function(d) { return d.birth; }).getTime());
+    earliest_date.setFullYear(earliest_date.getFullYear() - 10)
 
-    earliest_date = d3.min(data, function(d) { return d.birth; });
-    latest_date = d3.max(data, function(d) { return d.death; });
-    y.domain([earliest_date - 10, latest_date + 10]);
+    var latest_date = new Date(d3.max(data, function(d) { return d.death; }).getTime());
+    latest_date.setFullYear(latest_date.getFullYear() + 10)
 
-    var bar = chart.selectAll('g')
+    var x = d3.scaleBand()
+        .paddingOuter(0.1)
+        .paddingInner(0.2)
+        .domain(d3.map(data, function(d) { return d.name; }).keys())
+        .range([0, 100], 0.1);
+
+    var y = d3.scaleTime()
+        .domain([earliest_date, latest_date])
+        .range([height, 0]);
+
+    var y_axis = d3.axisLeft(y)
+
+    chart
+      .append('g')
+        .call(y_axis)
+
+    var bar = chart.selectAll('.bar')
         .data(data)
         .enter().append('g')
-        .attr('translate', function(d) { return 'translate(' + x(d.name) + ', 0)'; })
+        .attr('transform', function(d) { return `translate( ${x(d.name)} , 0)`; })
 
     bar.append('rect')
         .attr('y', function(d) { return y(d.death); })
         .attr('height', function(d) { return y(d.birth) - y(d.death); })
-        .attr('width', x.rangeBand())
+        .attr('width', x.bandwidth())
         .attr('fill', 'steelblue')
 });
