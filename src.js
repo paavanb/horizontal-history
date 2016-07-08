84760950a3e75ebe6b396f475b7f520fa644eac6
Paavan Bhavsar <pbhavsar@kyruus.com>
2016-07-07 23:39:45 -0400

    Reverse x and y in graph for readability

--- src.js
+++ src.js
@@ -9,12 +9,12 @@ function nearestTen(n) {
 }
 
 var margin = {
-    top: 10,
+    top: 50,
     right: 10,
     bottom: 10,
-    left: 50
+    left: 10
 }
-var width = 960, height = 500;
+var width = 600, height = 500;
 
 
 var chart = d3.select('.chart')
@@ -24,47 +24,46 @@ var chart = d3.select('.chart')
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.tsv("data.tsv", type, function(error, data) {
-    // Get upper and lower bounds of y axis, clamped to nearest ten for prettiness
+    // Get upper and lower bounds of x axis, clamped to nearest ten for prettiness
     var earliest_date = new Date(d3.min(data, function(d) { return d.birth; }).getTime());
     earliest_date.setFullYear(nearestTen(earliest_date.getFullYear() - 10))
 
     var latest_date = new Date(d3.max(data, function(d) { return d.death; }).getTime());
     latest_date.setFullYear(nearestTen(latest_date.getFullYear() + 10))
 
-    var x = d3.scaleBand()
+    var y = d3.scaleBand()
         .paddingOuter(0.1)
         .paddingInner(0.2)
         .domain(d3.map(data, function(d) { return d.name; }).keys())
         .range([0, 100], 0.1);
 
-    var y = d3.scaleTime()
+    var x = d3.scaleTime()
         .domain([earliest_date, latest_date])
-        .range([height, 0]);
+        .range([0, width]);
 
-    var y_axis = d3.axisLeft(y)
+    var x_axis = d3.axisTop(x)
 
     chart
       .append('g')
-        .call(y_axis)
+        .call(x_axis)
 
     // Create groups for bars
     var bars = chart.selectAll('.bar')
         .data(data)
       .enter().append('g')
         .attr('class', 'bar')
-        .attr('transform', function(d) { return `translate( ${x(d.name)} , ${y(d.death)})`; })
+        .attr('transform', function(d) { return `translate( ${x(d.birth)} , ${y(d.name)})`; })
 
     // Style bars
     bars.append('rect')
-        .attr('height', function(d) { return y(d.birth) - y(d.death); })
-        .attr('width', x.bandwidth())
+        .attr('width', function(d) { return x(d.death) - x(d.birth); })
+        .attr('height', y.bandwidth())
         .attr('fill', 'steelblue')
 
     // Style text on bars
     bars.append('text')
         .text(function(d) { return d.name })
-        .attr('transform', 'rotate(90)')
-        .attr('dy', -x.bandwidth()/2 + 5)
+        .attr('dy', y.bandwidth()/2 + 5)
         .attr('dx', 10)
         .attr('fill', 'white')
         .attr('font-family', 'Helvetica')
